a4452d31f90258968f8c4e724f78ac000feaafa3
minjeonglim <oij>
2018-12-10 19:57:03 +0000

    added connected streams example

--- chapter4/src/main/java/com/github/churtado/flink/com/github/churtado/flink/exercises/ConnectStream.java
+++ chapter4/src/main/java/com/github/churtado/flink/com/github/churtado/flink/exercises/ConnectStream.java
@@ -5,6 +5,7 @@ import com.github.churtado.flink.util.SensorSource;
 import com.github.churtado.flink.util.SensorTimeAssigner;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.ConnectedStreams;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -74,18 +75,35 @@ public class ConnectStream {
 //        }).print();
 
         // if done this way, all partitions by key will be sent to same operator instance
+        // operator instance would have access to keyed state
+//        ConnectedStreams<SensorReading, SensorReading> connected = first
+//                .connect(second)
+//                .keyBy("id", "id");
+//        connected.map(new CoMapFunction<SensorReading, SensorReading, Tuple4<String, String, Double, Long>>() {
+//            @Override
+//            public Tuple4<String, String, Double, Long> map1(SensorReading sensorReading) throws Exception {
+//                return new Tuple4<String, String, Double, Long>(sensorReading.id, sensorReading.location, sensorReading.temperature, sensorReading.timestamp);
+//            }
+//
+//            @Override
+//            public Tuple4<String, String, Double, Long> map2(SensorReading sensorReading) throws Exception {
+//                return new Tuple4<String, String, Double, Long>(sensorReading.id, sensorReading.location, sensorReading.temperature, sensorReading.timestamp);
+//            }
+//        }).print();
+
+        // In this instance, we broadcast the second, so it would be sent out to all operator instances
         ConnectedStreams<SensorReading, SensorReading> connected = first
-                .connect(second)
+                .connect(second.broadcast()) // this is the one difference from above!!!!
                 .keyBy("id", "id");
-        connected.map(new CoMapFunction<SensorReading, SensorReading, Tuple3<String, Double, Long>>() {
+        connected.map(new CoMapFunction<SensorReading, SensorReading, Tuple4<String, String, Double, Long>>() {
             @Override
             public Tuple4<String, String, Double, Long> map1(SensorReading sensorReading) throws Exception {
-                return new Tuple3<>(sensorReading.id, sensorReading.temperature, sensorReading.timestamp);
+                return new Tuple4<String, String, Double, Long>(sensorReading.id, sensorReading.location, sensorReading.temperature, sensorReading.timestamp);
             }
 
             @Override
             public Tuple4<String, String, Double, Long> map2(SensorReading sensorReading) throws Exception {
-                return new Tuple3<>(sensorReading.id, sensorReading.temperature, sensorReading.timestamp);
+                return new Tuple4<String, String, Double, Long>(sensorReading.id, sensorReading.location, sensorReading.temperature, sensorReading.timestamp);
             }
         }).print();
 
